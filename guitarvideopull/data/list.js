a9f987880b774da94f52757dfd5479183dfac4c2
parencode <51156091+parencode@users.noreply.github.com>
2024-01-29 20:01:26 +0100

    Update list.js
    Signed-off-by: parencode <51156091+parencode@users.noreply.github.com>

--- guitarvideopull/data/list.js
+++ guitarvideopull/data/list.js
@@ -1,1 +1,18 @@
+let videosJSON = {
+  "videos": [
+    {
+      "titulo": "AC/DC - You Shook Me All Night Long - Guitar Tab | Lesson | Cover | Tutorial",
+      "artista": "Mr. Tabs",
+      "enlace": "https://www.youtube.com/watch?v=Vj8EQqoizKk"
+    },{
+      "titulo": "Morrison’s Jig | FOLK METAL VERSION by Vincent Moretto",
+      "artista": "Vincent Moretto",
+      "enlace": "https://www.youtube.com/watch?v=szRUnUxeXFs"
+    },{
+      "titulo": "The Wellerman + TAB (Brian May Version - Guitar Tutorial)",
+      "artista": "Guillaume Vrac",
+      "enlace": "https://www.youtube.com/watch?v=i8SpYH1NF6U"
+    }
+  ]
+}
 
