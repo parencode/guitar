23790d69407f1d9731262e01aeb97ab54cd2c1f0
parencode <51156091+parencode@users.noreply.github.com>
2024-02-04 11:04:41 +0100

    Add files via upload
    Signed-off-by: parencode <51156091+parencode@users.noreply.github.com>

--- guitarvideopull/data/list1.js
+++ guitarvideopull/data/list1.js
@@ -1,1 +1,136 @@
-
+let videosJSON = {
+  "videos": [
+    {
+      "titulo": "AC/DC - You Shook Me All Night Long - Guitar Tab | Lesson | Cover | Tutorial",
+      "artista": "Mr. Tabs",
+      "enlace": "https://www.youtube.com/watch?v=Vj8EQqoizKk"
+    },
+    {
+      "titulo": "Scorpions - Rock You Like A Hurricane (cover by Sershen&Zaritskaya feat. @VioletOrlandi)",
+      "artista": "NOAPOLOGY",
+      "enlace": "https://www.youtube.com/watch?v=tQkqdkwFmcc"
+    },
+    {
+      "titulo": "AC/DC - You Shook Me All Night Long (cover by Sershen&Zaritskaya feat. Kim and Shturmak)",
+      "artista": "NOAPOLOGY",
+      "enlace": "https://www.youtube.com/watch?v=HMVCshhQQ10"
+    },
+    {
+      "titulo": "Sershen & Zaritskaya feat. Halocene Non-Stop Video Mix",
+      "artista": "scirpaul",
+      "enlace": "https://www.youtube.com/watch?v=nrxcDD7EzGc"
+    },
+    {
+      "titulo": "Megaraptor - Ievan Polkka (Metal Version)",
+      "artista": "Megaraptor",
+      "enlace": "https://www.youtube.com/watch?v=gGgFBxwRz_k"
+    },
+    {
+      "titulo": "Morrison’s Jig | FOLK METAL VERSION by Vincent Moretto",
+      "artista": "Vincent Moretto",
+      "enlace": "https://www.youtube.com/watch?v=szRUnUxeXFs"
+    },
+    {
+      "titulo": "Beast In Black - Dark Connection (Official Full Album Stream)",
+      "artista": "Beast In Black",
+      "enlace": "https://www.youtube.com/watch?v=7NyON-NzBr4"
+    },
+    {
+      "titulo": "Miss Monique - MiMo Podcast 042 [Melodic Techno & House DJ Mix]",
+      "artista": "Miss Monique",
+      "enlace": "https://www.youtube.com/watch?v=0AhFghYZQ74"
+    },
+    {
+      "titulo": "Levántate Y Lucha | Poderosa Música Orquestal De Batalla | El Poder De La Música Épica",
+      "artista": "Epic Music Mix",
+      "enlace": "https://www.youtube.com/watch?v=XNJf49nn0lw"
+    },
+    {
+      "titulo": "El Reno Renardo - Gente Mayor Del Metal (VejeStory Part I)(Videolyric)",
+      "artista": "ElRenoRenardo",
+      "enlace": "https://www.youtube.com/watch?v=1Hsm6WtDzzI"
+    },
+    {
+      "titulo": "Guardians of the Galaxy: Awesome Mix (Vol. 1 & Vol. 2) (Full Soundtrack)",
+      "artista": "Soul Records",
+      "enlace": "https://www.youtube.com/watch?v=Kt-tLuszKBA"
+    },
+    {
+      "titulo": "Thunderstruck - AC/DC & Dj Serj Moldova (mashup)",
+      "artista": "Dj Serj Moldova",
+      "enlace": "https://www.youtube.com/watch?v=74UcFPHGXO4"
+    },
+    {
+      "titulo": "AC/DC - Thunderstruck (Live At River Plate, December 2009)",
+      "artista": "AC/DC",
+      "enlace": "https://www.youtube.com/watch?v=n_GFN3a0yj0&list=PLsFQE5aB1i0kqyiqZL9G-dBicrI7rjg2J&index=7"
+    },
+    {
+      "titulo": "The Dead South - In Hell I'll Be In Good Company",
+      "artista": "The Dead South",
+      "enlace": "https://www.youtube.com/watch?v=B9FzVhw8_bY"
+    },
+    {
+      "titulo": "OFFICIAL Somewhere over the Rainbow - Israel \"IZ\" Kamakawiwoʻole",
+      "artista": "Mountain Apple Company In",
+      "enlace": "https://www.youtube.com/watch?v=V1bFr2SWP1I"
+    },
+    {
+      "titulo": "Guardians Of The Galaxy - Hooked On A Feeling",
+      "artista": "DarkRavenholm99",
+      "enlace": "https://www.youtube.com/watch?v=_ZKZ_lQ5FWQ"
+    },
+    {
+      "titulo": "Отава Ё - Финская полька (Otava Yo - Ievan polka)",
+      "artista": "Otava Yo",
+      "enlace": "https://www.youtube.com/watch?v=PX5OARoNFpg"
+    },
+    {
+      "titulo": "Morrison's Jig",
+      "artista": "Luar Na Lubre - Tema",
+      "enlace": "https://www.youtube.com/watch?v=glx-q9gMOqc"
+    },
+    {
+      "titulo": "Morrison's Jig",
+      "artista": "Drumspyder",
+      "enlace": "https://www.youtube.com/watch?v=bQepke7roxo"
+    },
+    {
+      "titulo": "Morrison's Jig",
+      "artista": "The Irish Experience",
+      "enlace": "https://www.youtube.com/watch?v=2RE-aD5lYdU"
+    },
+    {
+      "titulo": "Ring Of Fire - Johnny Cash & Dj Serj Moldova (remix)",
+      "artista": "Dj Serj Moldova",
+      "enlace": "https://www.youtube.com/watch?v=9VKjxWcd96s"
+    },
+    {
+      "titulo": "TNT - AC/DC & Dj Serj Moldova (mash up)",
+      "artista": "Dj Serj Moldova",
+      "enlace": "https://www.youtube.com/watch?v=4VP3kduCYRE"
+    },
+    {
+      "titulo": "Ram Jam - Black Betty",
+      "artista": "RamJamVEVO",
+      "enlace": "https://www.youtube.com/watch?v=I_2D8Eo15wE"
+    },
+    {
+      "titulo": "The HU - Wolf Totem (Official Music Video)",
+      "artista": "The HU",
+      "enlace": "https://www.youtube.com/watch?v=jM8dCGIm6yc"
+    },{
+      "titulo": "The Wellerman + TAB (Brian May Version - Guitar Tutorial)",
+      "artista": "Guillaume Vrac",
+      "enlace": "https://www.youtube.com/watch?v=i8SpYH1NF6U"
+    },{
+      "titulo": "WELLERMAN La Canción Pirata que se hizo viral en TikTok",
+      "artista": "Paola Hermosín",
+      "enlace": "https://www.youtube.com/watch?v=sFEw1grhqXg"
+    },{
+      "titulo": "The Wellerman (Sea Shanty) | Metal cover by Red Scorpion Studios",
+      "artista": "Red Scorpion Studios",
+      "enlace": "https://www.youtube.com/watch?v=2PAj1Va2JLA"
+    }
+  ]
+}
